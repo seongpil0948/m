af08080224c4507bfd0733d25094a861ee1b73a1
seongpil0948 <seongpil0948@gmail.com>
2020-08-15 09:06:36 +0900

    일단 완성.. ㅎㅎ

--- m_back/stock/core/classes/dual_momentum.py
+++ m_back/stock/core/classes/dual_momentum.py
@@ -6,24 +6,26 @@ from stock.models import Company, DailyPrice
  
 
 class DualMomentum(Market):
-    def __init__(self):
+    def __init__(self, start_date, end_date, stock_count):
         super().__init__() # for codes
         self.corpers = super().all_corp_info
+        self.start_date, self.end_date = get_times(start_date=start_date, end_date=end_date)
+        self.stock_count = stock_count
 
-    def get_rltv_momentum(self, start_date, end_date, stock_count):
+    @property
+    def rltv_momentum(self):
         """특정 기간 동안 수익률이 제일 높았던 stock_count 개의 종목들 (상대 모멘텀)
             - start_date  : 상대 모멘텀을 구할 시작일자 ('2020-01-01')   
             - end_date    : 상대 모멘텀을 구할 종료일자 ('2020-12-31')
             - stock_count : 상대 모멘텀을 구할 종목수
         """       
-        start_date, end_date = get_times(start_date=start_date, end_date=end_date)
-
+        
         # KRX 종목별 수익률을 구해서 2차원 리스트 형태로 추가
         rows = []
         columns = ['code', 'company', 'old_price', 'new_price', 'returns']        
         for i in self.codes:
-            old_query = DailyPrice.objects.filter(code=i, date=start_date).aggregate(close_price=Max('close_price'))
-            new_query = DailyPrice.objects.filter(code=i, date=end_date).aggregate(close_price=Max('close_price'))
+            old_query = DailyPrice.objects.filter(code=i, date=self.start_date).aggregate(close_price=Max('close_price'))
+            new_query = DailyPrice.objects.filter(code=i, date=self.end_date).aggregate(close_price=Max('close_price'))
 
             if new_query['close_price'] is  None or old_query['close_price'] is None: continue
 
@@ -37,77 +39,44 @@ class DualMomentum(Market):
         df = pd.DataFrame(rows, columns=columns)
         df = df[['code', 'company', 'old_price', 'new_price', 'returns']]
         df = df.sort_values(by='returns', ascending=False)
-        df = df.head(stock_count)
-        df.index = pd.Index(range(stock_count))
+        df = df.head(self.stock_count)
+        df.index = pd.Index(range(self.stock_count))
         print(df)
-        print(f"\nRelative momentum ({start_date} ~ {end_date}) : "\
+        print(f"\nRelative momentum ({self.start_date} ~ {self.end_date}) : "\
             f"{df['returns'].mean():.2f}% \n")
         return df
-a = DualMomentum()    
-a.get_rltv_momentum('2020-01-01', '2020-02-06', 2)
 
-    def get_abs_momentum(self, rltv_momentum, start_date, end_date):
+    @property
+    def abs_momentum(self):
         """특정 기간 동안 상대 모멘텀에 투자했을 때의 평균 수익률 (절대 모멘텀)
             - rltv_momentum : get_rltv_momentum() 함수의 리턴값 (상대 모멘텀)
             - start_date    : 절대 모멘텀을 구할 매수일 ('2020-01-01')   
             - end_date      : 절대 모멘텀을 구할 매도일 ('2020-12-31')
         """
-        stockList = list(rltv_momentum['code'])        
-        connection = pymysql.connect(host='localhost', port=3306, 
-            db='INVESTAR', user='root', passwd='******', autocommit=True)
-        cursor = connection.cursor()
-
-
-        # 사용자가 입력한 매수일을 DB에서 조회되는 일자로 변경 
-        sql = f"select max(date) from daily_price "\
-            f"where date <= '{start_date}'"
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        if (result[0] is None):
-            print ("{} -> returned None".format(sql))
-            return
-        start_date = result[0].strftime('%Y-%m-%d')
-
-
-        # 사용자가 입력한 매도일을 DB에서 조회되는 일자로 변경 
-        sql = f"select max(date) from daily_price "\
-            f"where date <= '{end_date}'"
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        if (result[0] is None):
-            print ("{} -> returned None".format(sql))
-            return
-        end_date = result[0].strftime('%Y-%m-%d')
-
-
-        # 상대 모멘텀의 종목별 수익률을 구해서 2차원 리스트 형태로 추가
+        rl_df = self.rltv_momentum
+        codes = rl_df['code']
         rows = []
         columns = ['code', 'company', 'old_price', 'new_price', 'returns']
-        for _, code in enumerate(stockList):            
-            sql = f"select close from daily_price "\
-                f"where code='{code}' and date='{start_date}'"
-            cursor.execute(sql)
-            result = cursor.fetchone()
-            if (result is None):
-                continue
-            old_price = int(result[0])
-            sql = f"select close from daily_price "\
-                f"where code='{code}' and date='{end_date}'"
-            cursor.execute(sql)
-            result = cursor.fetchone()
-            if (result is None):
-                continue
-            new_price = int(result[0])
+
+        for code in codes:
+
+            old_query = DailyPrice.objects.filter(code=i, date=self.start_date).aggregate(close_price=Max('close_price'))
+            new_query = DailyPrice.objects.filter(code=i, date=self.end_date).aggregate(close_price=Max('close_price'))
+
+            if new_query['close_price'] is  None or old_query['close_price'] is None: continue
+
+            old_price = int(old_query['close_price'])
+            new_price = int(new_query['close_price'])
+
             returns = (new_price / old_price - 1) * 100
-            rows.append([code, self.mk.codes[code], old_price, new_price,
-                returns])
+            rows.append([i, self.corpers[i].name_ko, 
+                old_price, new_price, returns])
 
 
         # 절대 모멘텀 데이터프레임을 생성한 후 수익률순으로 출력
         df = pd.DataFrame(rows, columns=columns)
         df = df[['code', 'company', 'old_price', 'new_price', 'returns']]
         df = df.sort_values(by='returns', ascending=False)
-        connection.close()
         print(df)
         print(f"\nAbasolute momentum ({start_date} ~ {end_date}) : "\
             f"{df['returns'].mean():.2f}%")
