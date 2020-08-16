9c95618a70e3d023e0c9ef06ba480f50fce2bd11
seongpil0948 <seongpil0948@gmail.com>
2020-08-16 15:37:34 +0900

    포기할랭

--- stock/core/policies/common.py
+++ stock/core/policies/common.py
@@ -1,11 +1,12 @@
 from stock.core.data import Market, get_times
 
 class BasePolicy(Market):
-    def __init__(self, budget=10000000, num_stocks=1000,
+    def __init__(self, budget=10000000, num_stocks=1000, stock_price=100,
         code='100220', start_date='2018-01-01', end_date='2019-01-01'):
         self.actions = ['buy', 'sell', 'hold']
         self.budget = budget # 실제로는 API 에서 제공받을 예정
         self.num_stocks = num_stocks
+        self.stock_price = stock_price
         
         super().__init__(code=code, start_date=start_date, end_date=end_date)
 
@@ -15,3 +16,14 @@ class BasePolicy(Market):
 
     def select_action(self):
         pass
+
+    def hi(self, acition):
+        if action not in self.actions:
+            print(f"{action} is not exist")
+        elif acition == 'buy':
+            self.num_stocks += self.budget / self.stock_price
+            self.budget = self.budget % self.stock_price
+            
+
+
+        
