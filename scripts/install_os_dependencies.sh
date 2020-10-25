25b72067c1edf188fb77c5c6d7a230588f383f05
2044smile <2044smile@naver.com>
2020-10-25 15:41:59 +0900

    test: codedeploy

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
 sudo apt-get install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
-sudo apt-get install -y gcc-c++ flex epel-release supervisor
-#sudo apt-get install -y gcc-c++ flex epel-release nginx supervisor
-#/etc/init.d/nginx stop
+sudo apt-get install -y gcc-c++ flex epel-release nginx supervisor
+/etc/init.d/nginx stop
