e5541964b4251d2c9661afad20e5551349e12dbb
seongpil0948 <seongpil0948@gmail.com>
2020-10-25 21:34:05 +0900

    make bolinget view

---
+++ stock/serializers/stock_tech/bolinger_band.py
@@ -0,0 +1,10 @@
+from stock.serializers.common.common import *
+from rest_framework import serializers
+
+__all__ = [
+    'BolingerBandSerializer',
+]
+
+class BolingerBandSerializer(Code, WindowSize):
+  class Meta:
+    ref_name = 'BolingerBandSerializer'
