7526743e6d8d0fae2ec184167a8595ee5c6286fc
2044smile <2044smile@naver.com>
2020-10-25 15:43:14 +0900

    test: codedeploy

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-chown ubuntu:ubuntu /home/ubuntu/app
+sudo chown ubuntu:ubuntu /home/ubuntu/app
 virtualenv /home/ubuntu/app/project-venv
-chown ubuntu:ubuntu /home/ubuntu/app/project-venv
-chown ubuntu:ubuntu /home/ubuntu/app/project-venv/*
-source /home/ubuntu/app/project-venv/bin/activate
+sudo chown ubuntu:ubuntu /home/ubuntu/app/project-venv
+sudo chown ubuntu:ubuntu /home/ubuntu/app/project-venv/*
+sudo source /home/ubuntu/app/project-venv/bin/activate
 pip install -r /home/ubuntu/app/requirements.txt
